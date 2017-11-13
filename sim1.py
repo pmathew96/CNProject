ef00a9b833b926c4bb7dd79e3a4e37dfa1aa1372
pmathew96 <paul.mathew@ashoka.edu.in>
2017-11-13 16:34:49 +0530

    Modified code for mutual inhibition

--- sim1.py
+++ sim1.py
@@ -24,14 +24,24 @@ while True:
     randNum2 = random.randint(0,1)
     N1.potential += randNum1
     N2.potential += randNum2
-    if N1.potential >= N1.threshold:
-        if N2.firing == 0:
+    if N1.potential >= N1.threshold & N2.potential >= N2.threshold:
+        randNum3 = random.randint(0,1)
+        if randNum3 == 0:
             N1.firing = 1
+            N2.potential -= 1
             N3.potential += 1
-    if N2.potential >= N1.threshold:
-        if N1.firing == 0:
+        if randNum3 == 1:
             N2.firing = 1
-            N4.potential += 1
+            N1.potential -= 1
+            N3.potential += 1
+    elif N1.potential >= N1.threshold:
+        N1.firing = 1
+        N2.potential -= 1
+        N3.potential += 1
+    elif N2.potential >= N2.threshold:
+        N2.firing = 1
+        N1.potential -= 1
+        N4.potential += 1
     #ret = str(N1.firing) + ',' + str(N2.firing)
     #f.write(ret+'\n')
     print('(' + str(N1.firing) + ',' + str(N2.firing) + ')')
@@ -39,15 +49,15 @@ while True:
     if N1.firing == 1:
         N1.potential -= 1
         N1.firing = 0
-        if N3.potential == N3.threshold:
-            N1.potential = -70
-            N3.potential = -70
+        # if N3.potential == N3.threshold:
+        #     N1.potential = -70
+        #     N3.potential = -70
     if N2.firing == 1:
         N2.potential -= 1
         N2.firing = 0
-        if N4.potential == N4.threshold:
-            N2.potential = -70
-            N4.potential = -70
+        # if N4.potential == N4.threshold:
+        #     N2.potential = -70
+        #     N4.potential = -70
     time.sleep(1)
 
 #f.close()
